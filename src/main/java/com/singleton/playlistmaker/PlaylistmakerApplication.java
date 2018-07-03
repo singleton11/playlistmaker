8a6ea9f6b5f9fa5045826720cf12330e2b691808
Anton Prokhorov <betrayer11@gmail.com>
2018-07-03 23:35:28 +0300

    Initial commit

---
+++ src/main/java/com/singleton/playlistmaker/PlaylistmakerApplication.java
@@ -0,0 +1,18 @@
+package com.singleton.playlistmaker;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistmakerApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaylistmakerApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Test");
+    }
+}
